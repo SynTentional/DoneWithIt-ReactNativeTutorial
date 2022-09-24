11db4eeff0605cb7e93ec5f983dacae65384fdb9
Chris <cmullins318@gmail.com>
2022-09-24 08:28:53 -0700

    obligatory minor change commit

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,11 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
     console.log("App executed"),
 
-    
+
     // View -> AndroidView
     <View style={styles.container}>
       <Text>Hello React Native!</Text>
@@ -16,8 +16,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    flex: 1, //Means take up as much space as you can
+    backgroundColor: 'dodgerblue', // Can use named colors or RGB colors as well
     alignItems: 'center',
     justifyContent: 'center',
   },
