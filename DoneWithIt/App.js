5bbbf8637952056d86f246745e11e79db79a70f1
Chris <cmullins318@gmail.com>
2022-09-24 11:20:56 -0700

    learned about images and toucable images

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, TouchableWithoutFeedback, Image, SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
@@ -8,11 +8,15 @@ export default function App() {
 
     // View -> AndroidView
     <SafeAreaView style={styles.container}>
-      <Text>Hello React Native!</Text> 
-      <Image source={{ 
+      <Text>Hello React Native!</Text>
+      <TouchableWithoutFeedback onPress={() => console.log('Image tapped')}>
+        <Image source={{ 
+        
         width: 200,
         height: 300,
         uri: "https://picsum.photos/200/300" }}/> 
+      </TouchableWithoutFeedback> 
+      
       <StatusBar style="auto" />
     </SafeAreaView>
   );
